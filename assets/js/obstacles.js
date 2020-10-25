7fee4c529a11401d11ea4d806cde3f5ddd2e6722
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-25 21:19:37 +0000

    Update: increase width and bottom radius of obstacles

--- assets/js/obstacles.js
+++ assets/js/obstacles.js
@@ -3,9 +3,9 @@ const obstaclesArray = [];
 class Obstacle {
     constructor() {
         this.top = (Math.random() * canvas.height / 3);
-        this.bottom = (Math.random() * canvas.height / 3);
+        this.bottom = (Math.random() * canvas.height / 2.5);
         this.x = canvas.width;
-        this.width = 20;
+        this.width = 80;
         this.color = "hsla(" + hue + ",100%, 50%)";
         this.counted = false;
     }
@@ -28,7 +28,7 @@ class Obstacle {
 function handleObstacles() {
     // everytime framecount hits a number that is divisible by 50, enter this if statement code block
     // execute this if statement every 50 frames
-    if (frame % 150 === 0) {
+    if (frame % 200 === 0) {
         obstaclesArray.unshift(new Obstacle);   
     } 
     for (let i = 0; i < obstaclesArray.length; i++) {
