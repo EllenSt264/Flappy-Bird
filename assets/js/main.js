38abf27d447a485f8f60d9bb57394defcd19cae5
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-25 18:24:22 +0000

    Add feat: animate function to move the bird later

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,23 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = 60;
+canvas.width = 600;
 canvas.height = 400;
 
-// flips between true and false whenever spacebar is pressed
-let spacePressed = false;
-// uses the math dot side method to make our bird move up and down slightly when idle
-let angle = 0
-// helps cycle through rgb color spectrum
-let hue = 0;
-// keeps track of frame count of the animation loop so we can add periodic trigger to our game
-// will mainly be used to set interval in which obstacles appear
-let frame = 0;
+
+let spacePressed = false;   // flips between true and false whenever spacebar is pressed
+let angle = 0               // uses the math dot side method to make our bird move up and down slightly when idle
+let hue = 0;         // helps cycle through rgb color spectrum
+let frame = 0;      // frame keeps track of frame count of the animation loop so we can add periodic trigger to our game, will mainly be used to set interval in which obstacles appear
 let score = 0;
-// can use gamespeed to move obstacles, particles and background at the same speed
-// or add multipliers to create parallax effect 
-// with parallax everything moves at a slightly different speed but relative to each other
+/* We can use gamespeed to move obstacles, particles and background at the same speed
+or add multipliers to create parallax effect 
+with parallax everything moves at a slightly different speed but relative to each other */
 let gamespeed = 2;
 
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(10, 10, 50, 50);    // draws a rectangle that will represent our player, for now, at coordinates 10 10 and width/height of 50
+    requestAnimationFrame(animate);
+}
+
+animate();
